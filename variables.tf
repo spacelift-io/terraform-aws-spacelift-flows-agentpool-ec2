95d3dbaf936da4ae331c049049f826cff7776de6
ilya-hontarau <ilyah@spacelift.io>
2026-01-16 17:36:11 +0100

    feat: pass certs and proxy to agent

--- variables.tf
+++ variables.tf
@@ -92,3 +92,15 @@ variable "flows_docker_runtime_image" {
   type        = string
   default     = null
 }
+
+variable "custom_ca_certificates" {
+  description = "Custom CA certificates to inject into docker containers"
+  type        = string
+  default     = null
+}
+
+variable "http_proxy" {
+  description = "HTTP proxy URL for docker containers"
+  type        = string
+  default     = null
+}
